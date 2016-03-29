6e025a81328b66d54f70fde98633d98b81d1b619
Tobias94Olsson <dat14tol@student.lu.se>
2016-03-29 16:30:12 +0200

    Added writeSMToFile() and implemented.

--- Labb1/src/stableMarriage/StableMarriage.java
+++ Labb1/src/stableMarriage/StableMarriage.java
@@ -2,6 +2,7 @@ package stableMarriage;
 
 import java.awt.List;
 import java.util.LinkedList;
+import java.io.*;
 
 public class StableMarriage {
 	LinkedList<Man> manList;
@@ -13,12 +14,22 @@ public class StableMarriage {
 	public void readCouplesFromFile(String path) {
 
 	}
-
-	public void writeStableMatchingToFile(String path) {
-		
+	
+	//Printar till filen path
+	public void writeSMToFile(String path) {
+		try{
+			BufferedWriter out = new BufferedWriter(new FileWriter(path));
+			for (Man m : manList){
+				out.write(m.toString() + "\n");
+				//out.newLine(); Oklart om detta behövs
+			}
+			out.close();
+		}catch (IOException e){
+			
+		}
 	}
 	//Printar i konsolen
-	public void writeToConsole(){
+	public void writeSMToConsole(){
 		for (Man m : manList){
 			System.out.println(m.toString() + "\n");
 		}
