4feb613f0c1eedef1360a6721dffed5703474399
Tobias94Olsson <dat14tol@student.lu.se>
2016-03-29 14:49:20 +0200

    Added a LinkedList<Men> to StableMarriage().

--- Labb1/src/stableMarriage/StableMarriage.java
+++ Labb1/src/stableMarriage/StableMarriage.java
@@ -1,10 +1,12 @@
 package stableMarriage;
 
 import java.awt.List;
+import java.util.LinkedList;
 
 public class StableMarriage {
+	LinkedList<Man> manList;
 	public StableMarriage(){
-
+		manList = new LinkedList<Man>();
 	}
 	public void readCouplesFromFile(String path){
 		
