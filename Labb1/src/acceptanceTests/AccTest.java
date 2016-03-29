de0f288a41bc3d8b891d3fe7681c26ca27737162
Povel Larsson <povel.larsson@gmail.com>
2016-03-29 13:28:57 +0200

    Added testfiles

---
+++ Labb1/src/acceptanceTests/AccTest.java
@@ -0,0 +1,14 @@
+package acceptanceTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
