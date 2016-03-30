c130ceb845ef1a0c7f88c18fd434be95127b4d48
Povel Larsson <povel.larsson@gmail.com>
2016-03-30 11:34:52 +0200

    Added some tests

--- Labb1/src/acceptanceTests/AccTest.java
+++ Labb1/src/acceptanceTests/AccTest.java
@@ -110,4 +110,24 @@ public class AccTest extends TestCase {
 			line2 = br2.readLine();
 		}
 	}
+	//Inte ett test. Bara konstig körmiljö.
+	@Test
+	public void testbbt() throws IOException {
+		String[] arguments = new String[] {
+				"testFiles/data/sm-bbt-in.txt",
+				"output/java-bbt-out.txt" };
+
+		Main.main(arguments);
+		BufferedReader br1 = new BufferedReader(new FileReader(
+				"testFiles/data/sm-friends-out.txt"));
+		BufferedReader br2 = new BufferedReader(new FileReader(
+				"output/java-bbt-out.txt"));
+		String line1 = br1.readLine();
+		String line2 = br2.readLine();
+		while ((line1 != null) || (line2 != null)) {
+			//assertEquals("De är inte korrekt gifta", line1, line2);
+			line1 = br1.readLine();
+			line2 = br2.readLine();
+		}
+	}
 }
