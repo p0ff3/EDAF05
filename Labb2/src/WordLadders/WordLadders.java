516a2e3c560ba87590d76ce70f56adc29cd77be1
Tobias94Olsson <dat14tol@student.lu.se>
2016-03-31 21:15:12 +0200

    Added some methods that I think are needed/wanted in WordLadders()

--- Labb2/src/WordLadders/WordLadders.java
+++ Labb2/src/WordLadders/WordLadders.java
@@ -14,5 +14,19 @@ public class WordLadders {
 		Parser p = new Parser(path);
 		words = p.Parse();
 	}
+	
+	//Spelling TODO
+	public boolean isNeighbors(String s1, String s2){
+		return false;
+	}
+	// TODO
+	public int findClosestPath(String s1, String s2){
+		return 0;
+	}
+	
+	// TODO
+	public void findAllClosestPaths(){
+		
+	}
 }
 
