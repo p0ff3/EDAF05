327c509b39ece3b8661ab0f0a5633636b8c7fc6f
Tobias94Olsson <dat14tol@student.lu.se>
2016-04-12 11:00:49 +0200

    Added some datastructures and methods

---
+++ Labb3/src/spanningUSA/City.java
@@ -0,0 +1,17 @@
+package spanningUSA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class City {
+
+	String name;
+	//Lista med grannstäder, key är distans, value är namnet på staden/skulle kunna ha object City också.
+	Map<Integer, String> neighborCities;
+	
+	public City(String name){
+		this.name = name;
+		neighborCities = new HashMap<Integer, String>();
+	}
+}
+	
--- Labb3/src/spanningUSA/spanningUSA.java
+++ Labb3/src/spanningUSA/spanningUSA.java
@@ -1,5 +1,17 @@
 package spanningUSA;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class spanningUSA {
 
+	Map<String, City> cities;
+	public spanningUSA(){
+		cities = new HashMap<String, City>();
+	}
+	
+	public void readCitiesFromFile(String path){
+		
+	}
+	
 }
