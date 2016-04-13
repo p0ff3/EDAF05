f8efcf6d9a8a31e7295e0c4945a83d013f71c4a2
Tobias94Olsson <dat14tol@student.lu.se>
2016-04-13 13:22:20 +0200

    Added testfiles

--- Labb3/src/spanningUSA/Main.java
+++ Labb3/src/spanningUSA/Main.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
-		Parser p = new Parser("test");
+		Parser p = new Parser("");
 		HashMap<String, City> cities = null;
 		try {
 			cities = p.Parse();
