88eef9bd8db794b2cd39a9f594b2a646b1cc9caf
Tobias94Olsson <dat14tol@student.lu.se>
2016-04-13 13:24:53 +0200

    Fixed testfiles

--- Labb3/src/spanningUSA/Main.java
+++ Labb3/src/spanningUSA/Main.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
-		Parser p = new Parser("");
+		Parser p = new Parser("highWays.txt");
 		HashMap<String, City> cities = null;
 		try {
 			cities = p.Parse();
@@ -16,7 +16,7 @@ public class Main {
 			System.out.println("Fel på pathen din bög.");
 		}
 		SpanningUSA spann = new SpanningUSA();
-		System.out.println(spann.getMstWeight(cities, "A"));
+		System.out.println(spann.getMstWeight(cities, "Sterling"));
 
 	}
 
