b70dc98cb4a8fa00c4554a4c9b42c7dfd92efba1
Tobias94Olsson <dat14tol@student.lu.se>
2016-05-03 16:11:08 +0200

    added parser

---
+++ Labb6/src/labb6/Parser.java
@@ -0,0 +1,34 @@
+package labb6;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Parser {
+	private String path;
+
+	public Parser(String path) {
+		this.path = path;
+	}
+
+	public String Parse() {
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
