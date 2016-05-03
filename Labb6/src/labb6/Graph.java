1b86dba3b1d67a9aad5ee77d88f4d8e9b8b93a42
Povel Larsson <povel.larsson@gmail.com>
2016-05-03 19:42:04 +0200

    Added some stuff

--- Labb6/src/labb6/Graph.java
+++ Labb6/src/labb6/Graph.java
@@ -6,31 +6,42 @@ public class Graph {
 	ArrayList<Node> Nodes = null;
 
 	public Graph(ArrayList<Node> NodeList) {
-
+		Nodes = NodeList;
 	}
 
 	public int getMaxFlow(String source, String sink) {
 		return 0;
 	}
 
-	public ArrayList<Edge> getMinCut(String source, String sink) {
+	public ArrayList<Edge> getMinCut(Node source, Node sink) {
+		ArrayList<Edge> path = null;
+		while (path != null) {
+			path = getAFlow(source, sink, new ArrayList<Edge>());
+			int minPotentialFlow = Integer.MAX_VALUE;
+			for(Edge e : path){
+				minPotentialFlow = Math.max(minPotentialFlow, e.getPotentialFlow());
+			}
+		}
+
 		return null;
 	}
 
-	private ArrayList<Edge> getAFlow(Node source, Node dest, ArrayList<Edge> path){
-		if(source == dest){
+	private ArrayList<Edge> getAFlow(Node source, Node dest,
+			ArrayList<Edge> path) {
+		if (source == dest) {
 			return null;
 		}
-		for(Edge e : source.getEdges()){
-			if(!path.contains(e)){
+		for (Edge e : source.getEdges()) {
+			if (!path.contains(e)) {
 				path.add(e);
-				ArrayList<Edge> result = getAFlow(e.getDestination(source), dest, path);
-				if(result != null){
+				ArrayList<Edge> result = getAFlow(e.getDestination(source),
+						dest, path);
+				if (result != null) {
 					return result;
 				}
 			}
 		}
-		
+
 		return null;
 	}
 
